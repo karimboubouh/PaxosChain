d26fa26484c3a486fecb35f1e9025ce4810bc1bf
abdelhak BOUAYAD <abdelhak.bouayad@um6p.ma>
2019-02-25 15:48:48 +0100

    project structur

---
+++ src/server/Server.java
@@ -0,0 +1,34 @@
+package server;
+
+import java.util.ArrayList;
+
+public class Server {
+	private int id;
+	private ArrayList<Object> blockchain;
+	private int []ballot;
+	private ArrayList<String> transaction ;
+	
+	
+	public void sendRequest() {
+		
+
+		
+	}
+	
+	public void receiveRequest() {
+		
+	}
+	
+	public void sendMessage() {
+		
+		
+		
+	}
+	
+	public void receiveMesage() {
+		
+		
+	}
+	
+	
+}
