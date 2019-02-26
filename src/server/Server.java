38a6cffb096b7becd8c757f15dde816d1ffc0e2e
boubouhkarim <boubouh.karim@gmail.com>
2019-02-26 10:25:30 +0100

    Communication Protocol

--- src/server/Server.java
+++ src/server/Server.java
@@ -3,32 +3,29 @@ package server;
 import java.util.ArrayList;
 
 public class Server {
-	private int id;
-	private ArrayList<Object> blockchain;
-	private int []ballot;
-	private ArrayList<String> transaction ;
-	
-	
-	public void sendRequest() {
-		
+    private int id;
+    private ArrayList<Object> blockchain;
+    private int[] ballot;
+    private ArrayList<String> transaction;
 
-		
-	}
-	
-	public void receiveRequest() {
-		
-	}
-	
-	public void sendMessage() {
-		
-		
-		
-	}
-	
-	public void receiveMesage() {
-		
-		
-	}
-	
-	
+
+    public void sendRequest() {
+
+    }
+
+    public void receiveRequest() {
+
+    }
+
+    public void sendMessage() {
+
+
+    }
+
+    public void receiveMesage() {
+
+
+    }
+
+
 }
